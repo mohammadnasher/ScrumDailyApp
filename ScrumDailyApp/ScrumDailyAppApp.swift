d7ecc665c255c0341e6523a861d1d891fb697c2d
monash <mohammad.nasher.mn@gmail.com>
2023-09-16 16:42:31 +0330

    add project files

---
+++ ScrumDailyApp/ScrumDailyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDailyAppApp.swift
+//  ScrumDailyApp
+//
+//  Created by Mo Nash on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDailyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
